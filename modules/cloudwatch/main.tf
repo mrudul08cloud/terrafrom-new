db54d9ffab67f213581109a5338be536aebcc411
Mrudul Lole <mrudullole08@gmail.com>
2026-02-18 20:05:14 +0530

    Add CloudWatch metric alarm for CPU utilization

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,1 +1,11 @@
 
+resource "aws_cloudwatch_metric_alarm" "cpu" {
+  alarm_name          = "cpu-alarm-${terraform.workspace}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+}
