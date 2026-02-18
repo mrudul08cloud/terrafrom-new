c4f502b6bf2cde39fdf570636d1c98a323ec23f5
Mrudul Lole <mrudullole08@gmail.com>
2026-02-18 20:15:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "env/${terraform.workspace}/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-west-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
   }
